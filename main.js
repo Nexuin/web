a9fb00b1777a6f5beb1a049365a892dfbfa998a1
Nexuin <bastiantova@gmail.com>
2024-11-20 20:03:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam Eca", "Malam Sayangg", "I Love You Aqilla Naisya Zafiran"],
+        typeSpeed: 70
+    });
+}
